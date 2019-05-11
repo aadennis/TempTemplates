67d47c7293a6e8a1ea81ac2bc29bcda4de555019
Dennis Wells <aadennis@users.noreply.github.com>
2019-05-11 13:23:52 +0100

    Update MefTemplates.cs

--- MEF/MefTemplates.cs
+++ MEF/MefTemplates.cs
@@ -24,3 +24,63 @@ using System.ComponentModel.Composition;
 
 // above the class (if appropriate)
 [Export]
+
+// Full example in a single assembly:
+//Program.cs
+using System;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition;
+using System.Reflection;
+
+namespace MefTry01
+{
+    class Program {
+       
+        
+        static void Main(string[] args) {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            var container = new CompositionContainer(catalog);
+
+            var _house = new House();
+            container.ComposeParts(_house);
+            _house.EnableIOT();
+            Console.ReadKey();
+        }
+    }
+}
+
+// House.cs
+using System;
+using System.ComponentModel.Composition;
+
+namespace MefTry01
+{
+    class House {
+        [Import]
+        private Alarm _alarm;
+        internal void EnableIOT() {
+            Console.WriteLine("IOTing the house...");
+            _alarm.Activate();
+        }
+    }
+}
+
+// Alarm.cs
+using System;
+using System.ComponentModel.Composition;
+
+namespace MefTry01 { 
+    [Export]
+    class Alarm {
+        internal void Activate() {
+            Console.WriteLine("Alarm activated.");
+        }
+    }
+}
+
+// Expected output 
+// IOTing the house...
+// Alarm activated.
+ 
+ 
+ 
